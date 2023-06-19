39e8f1d7f400b11cc3e681d0b4e5c5b28b246c80
tristanvrnn <134289144+tristanvrnn@users.noreply.github.com>
2023-06-19 14:29:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ShowMoney showMoneyDelegate = ShowInDollars;
+        showMoneyDelegate += ShowInEuros;
+
+        decimal amount = 80.50m;
+
+        showMoneyDelegate(amount);
+    }
+
+    public delegate void ShowMoney(decimal amount);
+
+    public static void ShowInDollars(decimal amount)
+    {
+        Console.WriteLine("Amount in dollars: " + amount + "$");
+    }
+
+    public static void ShowInEuros(decimal amount)
+    {
+        Console.WriteLine("Amount in euros: " + amount + "€");
+    }
+}
